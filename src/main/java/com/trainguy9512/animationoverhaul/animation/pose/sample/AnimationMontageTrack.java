ee253a9c8cb45dfbab052068f4a6f872bc4cb4ce
James Pelter <trainguy9512@gmail.com>
2025-02-12 23:20:46 -0600

    Finished montage rework

--- src/main/java/com/trainguy9512/animationoverhaul/animation/pose/sample/AnimationMontageTrack.java
+++ src/main/java/com/trainguy9512/animationoverhaul/animation/pose/sample/AnimationMontageTrack.java
@@ -1,6 +1,7 @@
 package com.trainguy9512.animationoverhaul.animation.pose.sample;
 
 import com.google.common.collect.Maps;
+import com.trainguy9512.animationoverhaul.animation.data.AnimationDataKey;
 import com.trainguy9512.animationoverhaul.animation.data.AnimationDriverContainer;
 import com.trainguy9512.animationoverhaul.animation.data.AnimationSequenceData;
 import com.trainguy9512.animationoverhaul.animation.data.PoseSamplerStateContainer;
@@ -9,18 +10,17 @@ import com.trainguy9512.animationoverhaul.animation.pose.JointSkeleton;
 import com.trainguy9512.animationoverhaul.util.time.Easing;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import java.util.ArrayList;
-import java.util.HashMap;
+
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class AnimationMontageTrack extends PoseSampler implements SampleableFromInput {
 
-    private final ArrayList<AnimationMontage> activeMontages = new ArrayList<AnimationMontage>();
-    private final HashMap<MontageConfiguration, Float> montages;
+    private final LinkedHashMap<MontageConfiguration, Float> montages;
 
     protected AnimationMontageTrack(Builder<?> builder) {
         super(builder);
-        this.montages = Maps.newHashMap();
+        this.montages = Maps.newLinkedHashMap();
     }
 
     public static Builder<?> builder(){
@@ -41,92 +41,59 @@ public class AnimationMontageTrack extends PoseSampler implements SampleableFrom
 
     @Override
     public void tick(AnimationDriverContainer animationDriverContainer, PoseSamplerStateContainer poseSamplerStateContainer){
-
-        this.montages.keySet().forEach((key) -> this.montages.compute(key, (i, value) -> (value == null ? 0 : value) + key.playRate()));
-
-        List<MontageConfiguration> finishedMontages = this.montages.keySet().stream().filter((key) -> this.montages.get(key) == 0).toList();
-        finishedMontages.forEach(this.montages::remove);
-
-       //TODO: Remove this vvvv
-
-        // Only run if there's actually montages currently loaded
-        if(this.hasActiveMontages()){
-            ArrayList<AnimationMontage> montagesToRemove = new ArrayList<>();
-            for(AnimationMontage animationMontage : activeMontages){
-                animationMontage.tick();
-                // If the montage has a weight of 0, add it to the list of montages to remove
-                if(animationMontage.getBlendWeightEased() == 0){
-                    montagesToRemove.add(animationMontage);
-                }
-            }
-            for(AnimationMontage animationMontage : montagesToRemove){
-                this.activeMontages.remove(animationMontage);
-            }
-        }
-
+        // Increment the elapsed time of each active montage
+        this.montages.forEach((configuration, timeElapsed) -> this.montages.put(configuration, (timeElapsed == null ? 0 : timeElapsed) + configuration.playRate()));
+        // Remove any montage that has an elapsed time greater than the montage's length
+        this.montages.keySet().stream().filter((configuration) -> this.montages.get(configuration) > configuration.getLength()).forEach(this.montages::remove);
     }
 
     @Override
     public AnimationPose sample(AnimationDriverContainer animationDriverContainer, PoseSamplerStateContainer poseSamplerStateContainer, JointSkeleton jointSkeleton, AnimationPose inputPose) {
-        // Initialize the animation pose
-        AnimationPose animationPose = AnimationPose.of(jointSkeleton);
-
-        // Only do this stuff if there's any loaded animation montages
-        if(this.hasActiveMontages()){
-            // Iterate over each montage and get the blended animation between top and bottom layers
-            for(int i = 0; i < this.activeMontages.size(); i++){
-                AnimationMontage animationMontage = this.activeMontages.get(i);
-                if(i == 0){
-                    animationPose = animationMontage.getAnimationPose(jointSkeleton);
-                } else {
-                    animationPose.blendLinear(animationMontage.getAnimationPose(jointSkeleton), animationMontage.getBlendWeightEased()
-                    );
-                }
-            }
-            float totalBlendWeight = 0;
-            for(AnimationMontage animationMontage : this.activeMontages){
-                totalBlendWeight = Mth.clamp(totalBlendWeight + animationMontage.getBlendWeightEased(), 0, 1);
-            }
-            //AnimationOverhaulMain.LOGGER.info("{}, {}", this.activeMontages.get(0).timeElapsed, totalBlendWeight);
-            animationPose = inputPose.getBlendedLinear(animationPose, totalBlendWeight);
-        } else {
-            return inputPose;
+        if(!this.montages.isEmpty()){
+            AnimationPose pose = new AnimationPose(inputPose);
+            this.montages.forEach((configuration, timeElapsed) -> pose.blend(AnimationPose.fromAnimationSequence(jointSkeleton, configuration.animationSequence, (timeElapsed + configuration.startTime) / AnimationSequenceData.INSTANCE.get(configuration.animationSequence).frameLength()), configuration.getWeight(timeElapsed), Easing.LINEAR));
+            return pose;
         }
-        return animationPose;
-    }
-
-    public boolean hasActiveMontages(){
-        return !this.activeMontages.isEmpty();
+        return inputPose;
     }
 
     /**
      * Plays the given montage. Should be accessed within the tick phase for intended functionality
      *
-     * @param animationMontage the animation montage
+     * @param montageConfigurationKey   Data key for a montage configuration
      */
-    public void playMontage(AnimationMontage animationMontage){
+    public void playMontage(AnimationDataKey<MontageConfiguration> montageConfigurationKey){
         // If the length of the montage is 2, remove entry 0 (the back layer)
-        if(activeMontages.size() == 2){
-            activeMontages.removeFirst();
+        if(this.montages.size() == 2){
+            this.montages.remove(this.montages.firstEntry().getKey());
         }
 
         // Add the montage to the front layer
-        activeMontages.add(animationMontage.copy());
-
-        // If there is a back layer, force deactivate the back layer based on the front layer's blend in time
-        if(activeMontages.size() == 2){
-            activeMontages.get(0).forceInactive(activeMontages.get(1).getBlendDuration(true));
-        }
+        this.montages.putLast(montageConfigurationKey.createInstance(), 0f);
     }
 
-    public record MontageConfiguration(ResourceLocation animationSequence, float startTime, float endTime, float playRate, float transitionInDuration, float transitionOutDuration, Easing transitionInEasing, Easing transitionOutEasing){
+    public record MontageConfiguration(ResourceLocation animationSequence, float startTime, float endTime, float playRate, float transitionInDuration, float transitionOutDuration, Easing transitionInEasing, Easing transitionOutEasing, float transitionOutStartTime){
 
         private MontageConfiguration(Builder builder){
-            this(builder.animationSequence, builder.startTime, builder.endTime, builder.playRate, builder.transitionInDuration, builder.transitionOutDuration, builder.transitionInEasing, builder.transitionOutEasing);
+            this(builder.animationSequence, builder.startTime, builder.endTime, builder.playRate, builder.transitionInDuration, builder.transitionOutDuration, builder.transitionInEasing, builder.transitionOutEasing, builder.transitionOutStartTime);
         }
 
         public static Builder builder(ResourceLocation animationSequence){
             return new Builder(animationSequence);
+        }
+
+        public float getLength(){
+            return (this.endTime - this.startTime) + (this.transitionOutStartTime * this.transitionOutDuration) + transitionOutDuration;
+        }
+
+        public float getWeight(float timeElapsed){
+            float offsetEndTransitionStartTime = this.getLength() - this.transitionOutDuration;
+            if(timeElapsed < this.transitionInDuration){
+                return this.transitionInEasing.ease(timeElapsed / this.transitionInDuration);
+            } else if(timeElapsed > offsetEndTransitionStartTime){
+                return this.transitionOutEasing.ease(1 - ((timeElapsed - offsetEndTransitionStartTime) / this.transitionOutDuration));
+            }
+            return 1;
         }
 
         public static class Builder {
@@ -139,6 +106,7 @@ public class AnimationMontageTrack extends PoseSampler implements SampleableFrom
             private float transitionOutDuration = 1;
             private Easing transitionInEasing = Easing.LINEAR;
             private Easing transitionOutEasing = Easing.LINEAR;
+            private float transitionOutStartTime = 0;
 
             private Builder(ResourceLocation animationSequence){
                 this.animationSequence = animationSequence;
@@ -170,38 +138,49 @@ public class AnimationMontageTrack extends PoseSampler implements SampleableFrom
             }
 
             /**
-             * Sets the easing used for either the in or out transition.
-             * @param easing        Easing interface to use.
-             * @param in            Whether the easing will be for the in transition or out transition. True = in, false = out.
+             * Sets the duration of either the in transition.
              */
-            public Builder setTransitionEasing(Easing easing, boolean in){
-                if(in){
-                    this.transitionInEasing = easing;
-                } else {
-                    this.transitionOutEasing = easing;
-                }
+            public Builder setTransitionInDuration(float duration){
+                this.transitionInDuration = duration;
                 return this;
             }
 
             /**
-             * Sets the duration of either the in or out transition.
-             * @param duration      Duration of the transition, in ticks.
-             * @param in            Whether the duration will be for the in transition or out transition. True = in, false = out.
+             * Sets the duration of either the out transition.
              */
-            public Builder setTransitionDuration(float duration, boolean in){
-                if(in){
-                    this.transitionInDuration = duration;
-                } else {
-                    this.transitionOutDuration = duration;
-                }
+            public Builder setTransitionOutDuration(float duration){
+                this.transitionOutDuration = duration;
                 return this;
             }
 
+            /**
+             * Sets the easing used for either the in transition.
+             */
+            public Builder setTransitionInEasing(Easing easing){
+                this.transitionInEasing = easing;
+                return this;
+            }
+
+            /**
+             * Sets the easing used for either the out transition.
+             */
+            public Builder setTransitionOutEasing(Easing easing){
+                this.transitionOutEasing = easing;
+                return this;
+            }
+
+            /**
+             * Sets the start time for the out transition.
+             * @param transitionOutStartTime    For the transition to end at the montage end time, this would be -1. For the montage to start at the end time, this would be 0.
+             */
+            public Builder setTransitionOutStartTime(float transitionOutStartTime){
+                this.transitionOutStartTime = transitionOutStartTime;
+                return this;
+            }
 
             public MontageConfiguration build(){
                 return new MontageConfiguration(this);
             }
         }
-
     }
 }
