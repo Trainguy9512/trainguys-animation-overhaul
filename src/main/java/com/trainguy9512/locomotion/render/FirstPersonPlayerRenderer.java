855c5ecc6f67d305c055a04f32013df1878b0e25
Trainguy9512 <trainguy9512@gmail.com>
2025-03-04 22:27:23 -0600

    Replaced MixinItemInHandRenderer with LocomotionFirstPersonPlayerRenderer

--- src/main/java/com/trainguy9512/locomotion/render/FirstPersonPlayerRenderer.java
+++ src/main/java/com/trainguy9512/locomotion/render/FirstPersonPlayerRenderer.java
@@ -2,15 +2,26 @@ package com.trainguy9512.locomotion.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
+import com.trainguy9512.locomotion.access.MatrixModelPart;
 import com.trainguy9512.locomotion.animation.animator.JointAnimatorDispatcher;
+import com.trainguy9512.locomotion.animation.animator.JointAnimatorRegistry;
 import com.trainguy9512.locomotion.animation.animator.entity.FirstPersonPlayerJointAnimator;
 import com.trainguy9512.locomotion.animation.joint.JointTransform;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.client.renderer.item.ItemModelResolver;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -31,6 +42,44 @@ public class FirstPersonPlayerRenderer {
     }
 
     public void render(float partialTicks, PoseStack poseStack, MultiBufferSource.BufferSource buffer, LocalPlayer playerEntity, int combinedLight) {
+        JointAnimatorDispatcher jointAnimatorDispatcher = JointAnimatorDispatcher.getInstance();
+
+        JointAnimatorDispatcher.getInstance().getFirstPersonPlayerDataContainer().ifPresent(
+                dataContainer -> jointAnimatorDispatcher.getInterpolatedFirstPersonPlayerPose().ifPresent(
+                        animationPose -> {
+
+                            JointTransform rightArmPose = animationPose.getJointTransform(FirstPersonPlayerJointAnimator.RIGHT_ARM_JOINT);
+                            JointTransform leftArmPose = animationPose.getJointTransform(FirstPersonPlayerJointAnimator.LEFT_ARM_JOINT);
+                            JointTransform rightHandPose = animationPose.getJointTransform(FirstPersonPlayerJointAnimator.RIGHT_HAND_JOINT);
+                            JointTransform leftHandPose = animationPose.getJointTransform(FirstPersonPlayerJointAnimator.LEFT_HAND_JOINT);
+
+                            poseStack.pushPose();
+                            poseStack.mulPose(Axis.ZP.rotationDegrees(180));
+
+
+                            AbstractClientPlayer abstractClientPlayer = this.minecraft.player;
+                            PlayerRenderer playerRenderer = (PlayerRenderer)this.entityRenderDispatcher.getRenderer(abstractClientPlayer);
+                            PlayerModel playerModel = playerRenderer.getModel();
+                            playerModel.resetPose();
+
+                            ((MatrixModelPart)(Object) playerModel.rightArm).locomotion$setMatrix(rightArmPose.getTransform());
+                            ((MatrixModelPart)(Object) playerModel.leftArm).locomotion$setMatrix(leftArmPose.getTransform());
+
+
+                            playerModel.rightArm.render(poseStack, buffer.getBuffer(RenderType.entityTranslucent(abstractClientPlayer.getSkin().texture())), combinedLight, OverlayTexture.NO_OVERLAY);
+                            playerModel.leftArm.render(poseStack, buffer.getBuffer(RenderType.entityTranslucent(abstractClientPlayer.getSkin().texture())), combinedLight, OverlayTexture.NO_OVERLAY);
+
+                            this.renderItem(abstractClientPlayer, dataContainer.getDriverValueInterpolated(FirstPersonPlayerJointAnimator.MAIN_HAND_ITEM, partialTicks), ItemDisplayContext.FIRST_PERSON_RIGHT_HAND, false, poseStack, rightHandPose, buffer, combinedLight);
+                            //this.renderItemInHand(abstractClientPlayer, ItemStack.EMPTY, poseStack, HumanoidArm.LEFT, animationPose, bufferSource, i);
+
+
+                            //playerRenderer.renderRightHand(poseStack, bufferSource, i, abstractClientPlayer);
+                            //poseStack.popPose();
+                            poseStack.popPose();
+                        }
+                )
+        );
+
         buffer.endBatch();
     }
 
@@ -45,6 +94,24 @@ public class FirstPersonPlayerRenderer {
 
                 poseStack.mulPose(cameraPose.getTransform().setTranslation(cameraPose.getTransform().getTranslation(new Vector3f().div(16f))));
             });
+        }
+    }
+
+
+
+    public void renderItem(LivingEntity entity, ItemStack itemStack, ItemDisplayContext displayContext, boolean isLeftHand, PoseStack poseStack, JointTransform jointTransform, MultiBufferSource buffer, int combinedLight) {
+        if (!itemStack.isEmpty()) {
+            poseStack.pushPose();
+            jointTransform.transformPoseStack(poseStack, 16f);
+
+            //TODO: this code needs to be replaced with proper joint transforms
+            poseStack.mulPose(Axis.XP.rotationDegrees(-90.0F));
+            poseStack.mulPose(Axis.YP.rotationDegrees(180.0F));
+            poseStack.translate((isLeftHand ? -0.5F : 0.5F) / 16.0F, 2F/16F, -10F/16F);
+
+
+            this.itemRenderer.renderStatic(entity, itemStack, displayContext, isLeftHand, poseStack, buffer, entity.level(), combinedLight, OverlayTexture.NO_OVERLAY, entity.getId() + displayContext.ordinal());
+            poseStack.popPose();
         }
     }
 }
